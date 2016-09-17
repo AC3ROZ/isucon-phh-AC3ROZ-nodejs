d1ced7ef895d6d52b98912b27fac12cdd26ec12f
AC3ROZ <iori_16n1300006@nnn.ed.jp>
2016-09-17 09:52:44 +0900

    Score: 47486093

--- app.js
+++ app.js
@@ -9,30 +9,7 @@ const pmysql = require('promise-mysql');
 
 const woothee = require('woothee');
 
-function html(){ return `<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="/index.css">
-    <title>PHH アクセスカウンター</title>
-    <script>
-    window.onload = function(){
-        domain = document.getElementById("domain").innerText;
-        document.getElementById("domain").innerText = Object.keys(JSON.parse(domain)).length;
-        os= document.getElementById("os").innerText;
-        document.getElementById("os").innerText = Object.keys(JSON.parse(os)).length;
-        browser = document.getElementById("browser").innerText;
-        document.getElementById("browser").innerText = Object.keys(JSON.parse(browser)).length;
-    }
-  </script>
-  </head>
-  <body>
-
-    <h1>PHH アクセスカウンター</h1>
-
-    <p>これまで、<b id="domain">${JSON.stringify(domains)}</b>個のドメイン、<b id="browser">${JSON.stringify(bro)}</b>種類のブラウザ、<b id="os">${JSON.stringify(oss)}</b>種類の OS のアクセスがありました。</p>
-  </body>
-</html>` };
+function html(){ return `<!DOCTYPE html><html> <head> <meta charset="UTF-8"> <link rel="stylesheet" href="/index.css"> <title>PHH アクセスカウンター</title> <script> window.onload = function(){ domain = document.getElementById("domain").innerText; document.getElementById("domain").innerText = Object.keys(JSON.parse(domain)).length; os= document.getElementById("os").innerText; document.getElementById("os").innerText = Object.keys(JSON.parse(os)).length; browser = document.getElementById("browser").innerText; document.getElementById("browser").innerText = Object.keys(JSON.parse(browser)).length; } </script> </head> <body> <h1>PHH アクセスカウンター</h1> <p>これまで、<b id="domain">${JSON.stringify(domains)}</b>個のドメイン、<b id="browser">${JSON.stringify(bro)}</b>種類のブラウザ、<b id="os">${JSON.stringify(oss)}</b>種類の OS のアクセスがありました。</p> </body></html>` };
 var app = express();
 var dict = [];
 var domains = {};
